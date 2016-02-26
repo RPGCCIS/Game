965c4724f88281750c737f0545eb410911acfd81
James Troup <jlt9768@g.rit.edu>
2016-02-26 18:03:36 -0500

    drawobject and layer

--- Fragments/Fragments/DrawableObject.cs
+++ Fragments/Fragments/DrawableObject.cs
@@ -10,6 +10,19 @@ namespace Fragments
 {
     abstract class DrawableObject
     {
+        private Rectangle rec;
+        private Texture2D texture;
+        public DrawableObject(int x, int y, int width, int height)
+        {
+            rec = new Rectangle(x, y, width, height);
+        }
+        //Properties for variables
+        public Rectangle Rec { get { return rec; } }
+        public Texture2D Texture { get { return texture; } set { texture = value; } }
+        public int X { get { return rec.X; } set { rec.X = value; } }
+        public int Y { get { return rec.Y; } set { rec.Y = value; } }
+        //Overrideable method for draw if we want to change how things are drawn
+        abstract public void Draw();
         
     }
 }
---
+++ Fragments/Fragments/Layer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+namespace Fragments
+{
+    class Layer
+    {
+        //LAyer Class
+        private float movementMulitplier;
+        private float updateable;
+        private List<DrawableObject> textures;
+        //Needs to bring in drawable objects and there coordinates
+        public Layer(List<DrawableObject> d)
+        {
+            textures = d;
+        }
+    }
+}
