85402402905b38f39a5b69c3ca7893a221eb84a8
Nick <nxb3051@rit.edu>
2016-03-08 20:04:51 -0500

    Added an enemy class

---
+++ Fragments/Fragments/DrawableObject/Character/Enemy.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Fragments
+{
+    class Enemy : Character
+    {
+        //enum that defines three different enemy levels
+        public enum Type
+        {
+            grunt,
+            boss,
+            final
+        }
+
+        //holds an instance's type
+        private Type level;
+
+        //gets the instance's type
+        public Type Level
+        {
+            get { return level; }
+            set { level = value; }
+        }
+
+        //initial constructor for the enemy class
+        public Enemy(Type type, int x, int y, int w, int h) : base(x, y, w, h)
+        {
+            level = type;
+        }
+
+        //a temporary draw method
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+        }
+    }
+}
