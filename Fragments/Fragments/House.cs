51f9ab2521a9d02fbd87ce2f210dceaaf3271e7d
James Troup <jlt9768@g.rit.edu>
2016-03-18 10:44:50 -0500

    Creation of House object

---
+++ Fragments/Fragments/House.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+
+
+namespace Fragments
+{
+    class House:DrawableObject
+    {
+        InteractableObject door;
+        public House(int x, int y,int width,int height,Texture2D texture, int xOff, int yOff) 
+            : base(x,y,width,height,texture)
+        {
+            door = new InteractableObject(width + xOff, height + yOff, 100, 200,null,"test1");
+        }
+    }
+}
