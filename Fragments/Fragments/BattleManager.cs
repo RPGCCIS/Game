17bac4fd51ae62b15dc1f002f5d3ff332ec64fd2
Benjamin <brpylko@gmail.com>
2016-05-04 18:44:15 -0400

    Added Try/Catch to SoundManager so the game will run on my system. Made lose state work in BattleManager

--- Fragments/Fragments/BattleManager.cs
+++ Fragments/Fragments/BattleManager.cs
@@ -143,147 +143,153 @@ namespace Fragments
 				case BattleState.Fight:
 
                     //Initialization stuff
-                    if (stateChange)
-                    {
-                        title.Name = "What will you do?";
+					if(stateChange)
+					{
+						title.Name = "What will you do?";
 
-                        title.Dialogue.Add("Attack");
-                        title.Dialogue.Add("Defend");
-                    }
+						title.Dialogue.Add("Attack");
+						title.Dialogue.Add("Defend");
+					}
 
-                    if (IsKeyPressed(kbState, oldKbState, Keys.Enter))
-                    {
-                        if (title.Dialogue.Selected == 0)
-                        {
-                            state = BattleState.Attack;
-                        }
-                        else if (title.Dialogue.Selected == 1)
-                        {
-                            state = BattleState.Defend;
-                        }
-                    }
-                    if (IsKeyPressed(kbState, oldKbState, Keys.Back))
-                    {
-                        state = BattleState.Player;
-                    }
-                    break;
+					if(IsKeyPressed(kbState, oldKbState, Keys.Enter))
+					{
+						if(title.Dialogue.Selected == 0)
+						{
+							state = BattleState.Attack;
+						}
+						else if(title.Dialogue.Selected == 1)
+						{
+							state = BattleState.Defend;
+						}
+					}
+					if(IsKeyPressed(kbState, oldKbState, Keys.Back))
+					{
+						state = BattleState.Player;
+					}
+					break;
 
-                case BattleState.Attack:
-                    int rawAtk = p.Atk - e.Def;
-                    int dealtAtk = Player.Attack(e, rawAtk);
-                    title.Name = "You swing your Sword! You deal " + dealtAtk + " damage!";
-                    state = BattleState.Paused;
-                    break;
+				case BattleState.Attack:
+					int rawAtk = p.Atk - e.Def;
+					int dealtAtk = Player.Attack(e, rawAtk);
+					title.Name = "You swing your Sword! You deal " + dealtAtk + " damage!";
+					state = BattleState.Paused;
+					break;
 
-                case BattleState.Defend:
-                    title.Name = "You raise your Shield!";
-                    Player.Defending = true;
-                    state = BattleState.Paused;
-                    break;
+				case BattleState.Defend:
+					title.Name = "You raise your Shield!";
+					Player.Defending = true;
+					state = BattleState.Paused;
+					break;
 
-                case BattleState.Run:
-                    title.Name = "You got away safely!";
-                    state = BattleState.Paused;   
-                    break;
+				case BattleState.Run:
+					title.Name = "You got away safely!";
+					state = BattleState.Paused;   
+					break;
 
-                case BattleState.Enemy:
-                    e.Act(title);
-                    state = BattleState.Paused;
+				case BattleState.Enemy:
+					e.Act(title);
+					state = BattleState.Paused;
 
-                    if(Player.Defending == true)
-                    {
-                        Player.Defending = false;
-                    }
-                    break;
+					if(Player.Defending == true)
+					{
+						Player.Defending = false;
+					}
+					break;
 
-                case BattleState.Win:
-                    title.Name = "You won!";
-                    state = BattleState.Paused;
-                        break;
+				case BattleState.Win:
+					title.Name = "You won!";
+					state = BattleState.Paused;
+					break;
 
-                case BattleState.Lose:
-                    title.Name = "You Lost!";
-                    state = BattleState.Paused;
-                    break;
+				case BattleState.Lose:
+					title.Name = "You Lost!";
+					state = BattleState.Paused;
+					break;
 
-                case BattleState.Paused:
-                    if (pauseTimer.Update(gameTime))
-                    {
-                        if (oldState == BattleState.Run || oldState == BattleState.Win || oldState == BattleState.Lose)
-                        {
-                            //This is essentially our "end" state
-                            GameManager.Instance.State = GameManager.GameState.Map;
-                            state = BattleState.Start;
-                            return;
-                        }
-                        else if (oldState == BattleState.Enemy)
-                        {
-                            if (p.IsAlive())
-                            {
-                                state = BattleState.Player;
-                            }
-                            else
-                            {
-                                state = BattleState.Lose;
-                            }
-                        }
-                        else if (!e.IsAlive())
-                        {
-                            state = BattleState.Win;
-                        }
-                        else
-                        {
-                            state = BattleState.Enemy;
-                        }
-                    }
+				case BattleState.Paused:
+					if(pauseTimer.Update(gameTime))
+					{
+						if(oldState == BattleState.Run || oldState == BattleState.Win)
+						{
+							//This is essentially our "end" state
+							GameManager.Instance.State = GameManager.GameState.Map;
+							state = BattleState.Start;
+							return;
+						}
+						else if(oldState == BattleState.Lose)
+						{
+							GameManager.Instance.State = GameManager.GameState.Over;
+							state = BattleState.Start;
+							return;
+						}
+						else if(oldState == BattleState.Enemy)
+						{
+							if(p.IsAlive())
+							{
+								state = BattleState.Player;
+							}
+							else
+							{
+								state = BattleState.Lose;
+							}
+						}
+						else if(!e.IsAlive())
+						{
+							state = BattleState.Win;
+						}
+						else
+						{
+							state = BattleState.Enemy;
+						}
+					}
 
-                    break;             
-            }
+					break;             
+			}
 
-            //Input
-            if (IsKeyPressed(kbState, oldKbState, Keys.W))
-            {
-                title.Dialogue.Previous();
-            }
-            else if (IsKeyPressed(kbState, oldKbState, Keys.S))
-            {
-                title.Dialogue.Next();
-            }
+			//Input
+			if(IsKeyPressed(kbState, oldKbState, Keys.W))
+			{
+				title.Dialogue.Previous();
+			}
+			else if(IsKeyPressed(kbState, oldKbState, Keys.S))
+			{
+				title.Dialogue.Next();
+			}
 
-            oldKbState = kbState;
+			oldKbState = kbState;
 
-            if (oldState == state)
-            {
-                stateChange = false;
-            }
-            else
-            {
-                stateChange = true;
-            }
+			if(oldState == state)
+			{
+				stateChange = false;
+			}
+			else
+			{
+				stateChange = true;
+			}
 
-            //Wipe?
-            if (stateChange)
-            {
-                Wipe(title);
-            }
-        }
+			//Wipe?
+			if(stateChange)
+			{
+				Wipe(title);
+			}
+		}
 
-        public void Draw(SpriteBatch spritebatch)
-        {
-                p.Draw(spritebatch);
-                e.Draw(spritebatch);
-                title.Draw(spritebatch);
-        }
+		public void Draw(SpriteBatch spritebatch)
+		{
+			p.Draw(spritebatch);
+			e.Draw(spritebatch);
+			title.Draw(spritebatch);
+		}
 
-        public bool IsKeyPressed(KeyboardState current, KeyboardState old, Keys k)
-        {
-            return (current.IsKeyDown(k) && old.IsKeyUp(k));
-        }
+		public bool IsKeyPressed(KeyboardState current, KeyboardState old, Keys k)
+		{
+			return (current.IsKeyDown(k) && old.IsKeyUp(k));
+		}
 
-        public void Wipe(Message m)
-        {
-            m.Dialogue.Clear();
-            m.Dialogue.Selected = 0;
-        }
-    }
+		public void Wipe(Message m)
+		{
+			m.Dialogue.Clear();
+			m.Dialogue.Selected = 0;
+		}
+	}
 }
--- Fragments/Fragments/SoundManager.cs
+++ Fragments/Fragments/SoundManager.cs
@@ -11,99 +11,113 @@ using Microsoft.Xna.Framework.Content;
 
 namespace Fragments
 {
-    /// <summary>
-    /// Music player!
-    /// 
-    /// ---- Songs ----
-    /// All Final Fantasy IV music (for testing/consistency)
-    /// 
-    /// Main menu - Prelude
-    /// Town      - Welcome to our town
-    /// Overworld - Main theme
-    /// Battle    - Fight 2
-    /// 
-    /// </summary>
-    class SoundManager
-    {
-        #region Singleton
-        private static SoundManager instance;
+	/// <summary>
+	/// Music player!
+	/// 
+	/// ---- Songs ----
+	/// All Final Fantasy IV music (for testing/consistency)
+	/// 
+	/// Main menu - Prelude
+	/// Town      - Welcome to our town
+	/// Overworld - Main theme
+	/// Battle    - Fight 2
+	/// 
+	/// </summary>
+	class SoundManager
+	{
+		#region Singleton
 
-        public static SoundManager Instance
-        {
-            get
-            {
-                if (instance == null)
-                {
-                    instance = new SoundManager();
-                }
+		private static SoundManager instance;
 
-                return instance;
-            }
-            set
-            {
-                instance = value;
-            }
-        }
-        #endregion
+		public static SoundManager Instance
+		{
+			get
+			{
+				if(instance == null)
+				{
+					instance = new SoundManager();
+				}
 
-        #region Variables
-        //Content
-        private ContentManager content;
+				return instance;
+			}
+			set
+			{
+				instance = value;
+			}
+		}
 
-        //Sound holders
-        private Dictionary<string, Song> songs;
-        private Dictionary<string, SoundEffect> soundEffects;
+		#endregion
 
-        private bool playing;
-        #endregion
+		#region Variables
 
-        #region Properties
+		//Content
+		private ContentManager content;
 
-        public ContentManager Content
-        {
-            set { content = value; }
-        }
+		//Sound holders
+		private Dictionary<string, Song> songs;
+		private Dictionary<string, SoundEffect> soundEffects;
 
-        #endregion
+		private bool playing;
 
-        #region Constructor
-        public SoundManager()
-        {
-            MediaPlayer.IsRepeating = true;
-            playing = false;
+		#endregion
 
-            songs = new Dictionary<string, Song>();
-            soundEffects = new Dictionary<string, SoundEffect>();
-        }
-        #endregion
+		#region Properties
 
-        //Methods
-        public void PlaySong(string file)
-        {
-            if (playing)
-            {
-                MediaPlayer.Stop();
-            }
+		public ContentManager Content
+		{
+			set
+			{
+				content = value;
+			}
+		}
 
-            //If the song wasn't used yet, load it in
-            if (!songs.ContainsKey(file))
-            {
-                songs.Add(file, content.Load<Song>(file));
-            }
-            
-            MediaPlayer.Play(songs[file]);
-        }
+		#endregion
 
-        public void PlaySoundEffect(string file)
-        {
-            //If the song wasn't used yet, load it in
-            if (!songs.ContainsKey(file))
-            {
-                soundEffects.Add(file, content.Load<SoundEffect>(file));
-            }
+		#region Constructor
 
-            soundEffects[file].Play();
-        }
+		public SoundManager()
+		{
+			MediaPlayer.IsRepeating = true;
+			playing = false;
 
-    }
+			songs = new Dictionary<string, Song>();
+			soundEffects = new Dictionary<string, SoundEffect>();
+		}
+
+		#endregion
+
+		//Methods
+		public void PlaySong(string file)
+		{
+			try
+			{
+				if(playing)
+				{
+					MediaPlayer.Stop();
+				}
+
+				//If the song wasn't used yet, load it in
+				if(!songs.ContainsKey(file))
+				{
+					songs.Add(file, content.Load<Song>(file));
+				}
+				MediaPlayer.Play(songs[file]);
+			}
+			catch(Exception)
+			{
+			}
+		}
+
+		public void PlaySoundEffect(string file)
+		{
+			//If the song wasn't used yet, load it in
+			if(!songs.ContainsKey(file))
+			{
+				soundEffects.Add(file, content.Load<SoundEffect>(file));
+			}
+
+			soundEffects[file].Play();
+		}
+
+	}
 }
